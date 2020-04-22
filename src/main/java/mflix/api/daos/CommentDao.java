c520977940a91c11e055efe9a2f78d38c070ba5d
Michael Rodas <michael.rodasacevedo@jumbo.com>
2020-04-22 11:13:32 +0200

    CommentDao mostActiveCommenters method is implemented (Ticket user report)

--- src/main/java/mflix/api/daos/CommentDao.java
+++ src/main/java/mflix/api/daos/CommentDao.java
@@ -1,6 +1,7 @@
 package mflix.api.daos;
 
 import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.result.DeleteResult;
@@ -18,8 +19,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import static com.mongodb.client.model.Aggregates.limit;
+import static com.mongodb.client.model.Aggregates.sortByCount;
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.set;
@@ -118,7 +122,7 @@ public class CommentDao extends AbstractMFlixDao {
         } catch (Exception e) {
             log.error("The was an error while deleting the user", e);
         }
-        return result != null && result.getDeletedCount() >0;
+        return result != null && result.getDeletedCount() > 0;
     }
 
     /**
@@ -130,12 +134,21 @@ public class CommentDao extends AbstractMFlixDao {
      */
     public List<Critic> mostActiveCommenters() {
         List<Critic> mostActive = new ArrayList<>();
-        // // TODO> Ticket: User Report - execute a command that returns the
+        // // Ticket: User Report - execute a command that returns the
         // // list of 20 users, group by number of comments. Don't forget,
         // // this report is expected to be produced with an high durability
         // // guarantee for the returned documents. Once a commenter is in the
         // // top 20 of users, they become a Critic, so mostActive is composed of
         // // Critic objects.
+
+        List<Bson> pipeline = new LinkedList<>();
+        //pipeline.add(group("$email", sum("count", 1L)));
+        //pipeline.add(sort(descending("count")));
+        pipeline.add(sortByCount("$email"));
+        pipeline.add(limit(20));
+
+        MongoCollection<Critic> criticCollection = db.getCollection(COMMENT_COLLECTION, Critic.class).withCodecRegistry(pojoCodecRegistry);
+        criticCollection.withReadConcern(ReadConcern.MAJORITY).aggregate(pipeline).into(mostActive);
         return mostActive;
     }
 }
